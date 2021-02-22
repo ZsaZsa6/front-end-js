089e9b690da9b6ca77416334873a4a9919fdb082
Ronda Cowen <cowenhome5@gmail.com>
2021-02-22 16:20:10 -0600

    column chart not working, took out map function. still working on chart

--- src/chart.js
+++ src/chart.js
@@ -2,24 +2,23 @@ class Chart {
     constructor(hours, minutes, createdAt, updatedAt) {
         this.hours = hours
         this.minutes = minutes
-        this.createdAt = createdAt
-        this.updatedAt = updatedAt
+        this.createdAt = new Date(createdAt)
+        this.updatedAt = new Date(updatedAt)
         this.feastHours = 24 - this.hours
-        debugger
+
     }
     calculations() {
-        this.createdAt = createdAt
-        this.hours = hours
+        const startDate = this.createdAt.getTime();
 
-        const startDate = createdAt.getDate();
-        // const endDate = this.updatedAt.getDate();
+        const endDate = this.updatedAt.getTime();
 
 
-        const pointsData = (startDate, hours)
-        const points = pointsData.map(d => {
-            return { x: d.startDate, y: d.hours };
-        })
-        buildChart(points).render();
+
+        const points = { x: startDate, y: this.hours }
+        //    debugger
+
+        this.buildChart(points)
+
     }
 
     render() {
@@ -28,45 +27,31 @@ class Chart {
     buildChart(points) {
 
         JSC.chart('chartDiv', {
+            // debug:true,
             type: 'column',
             yAxis: {
-                scale_type: 'stacked',
+                // scale_type: 'stacked',
                 label_text: 'Hours Fasted',
-                // yAxis_scale_type: 'stackedFull',
+                // yAxis_scale_type: 'stacked',
 
             },
             series: [{ points: points }],
             title_label_text: 'Your Fast History',
             xAxis_label_text: 'Days',
-            xAxis_scale_type: 'time',
+            // xAxis_scale_type: 'time',
             series: [
                 {
                     name: 'Fasting Time',
                     id: 'f1',
                     points: [
-                        { x: '', y: 10 },
-                        { x: '', y: 10 },
-                        { x: '', y: 20 },
-                        { x: '', y: 10 },
-                        { x: '', y: 20 }
+                        { x: '', y: '' },
+                        { x: '', y: '' },
+                        { x: '', y: '' },
+                        { x: '', y: '' },
+                        { x: '', y: '' }
                     ]
                 }
-
-                // {
-                //     name: 'Feasting Time',
-                //     id: 'f2',
-                //     points: [
-                //         { x: 'endDate', y: 14 },
-                //         { x: 'endDate', y: 14 },
-                //         { x: 'endDate', y: 4 },
-                //         { x: 'Thursday', y: 14 },
-                //         { x: 'Friday', y: 4 }
-                //     ]
-                // }
             ]
-
         });
     }
 }
-
-
